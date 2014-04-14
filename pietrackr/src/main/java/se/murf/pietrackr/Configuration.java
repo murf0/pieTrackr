2a300c95116ab2d289248533776731fef56b2ff5
Mikael Mellgren <mikael@murf.se>
2014-04-14 20:21:22 +0200

    Lots

---
+++ pietrackr/src/main/java/se/murf/pietrackr/Configuration.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package se.murf.pietrackr;
+
+import java.io.IOException;
+
+import joptsimple.OptionException;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+/**
+ * @author mikael
+ *
+ */
+public class Configuration {
+
+	/**
+	 * 
+	 */
+	private static final String HELP_OPTION = "help";
+	private static final String OPTION_PUSH = "pushtopic";
+	private static final String OPTION_SERVER = "server";
+	private static final String OPTION_PORT = "port";
+	private static final String OPTION_CLIENTID = "clientid";
+	private static OptionSet options = null;
+	public Configuration(String[] args) throws Exception {
+		// TODO Auto-generated constructor stub
+		
+		OptionParser parser = new OptionParser() {
+			{
+				accepts(HELP_OPTION, "Shows this help message.");
+				accepts(OPTION_PUSH, "Topic to push to.").withRequiredArg().describedAs("pushtopic").ofType(String.class);
+				accepts(OPTION_SERVER, "Server to publish to").withRequiredArg().describedAs("server").ofType(String.class);
+				accepts(OPTION_PORT, "ServerPort.").withRequiredArg().describedAs("port").ofType(String.class);
+				accepts(OPTION_CLIENTID, "Client ID.").withRequiredArg().describedAs("clientid").ofType(String.class);
+			}
+		};
+		/*
+		 * OptionSet options = null;
+		 */
+		try {
+			options = parser.parse(args);
+		} catch (OptionException oe) {
+			System.out.println(oe.toString());
+			printUsage(parser);
+			System.exit(1);
+		}
+		if (!options.hasArgument(OPTION_PUSH) || !options.hasArgument(OPTION_SERVER) || !options.hasArgument(OPTION_PORT)) {
+			printUsage(parser);
+			System.exit(1);
+		}
+	}
+	
+	private static void printUsage(OptionParser parser) throws IOException {
+		System.out.println("Usage:");
+		System.out.println("      ");
+		parser.printHelpOn(System.out);
+	}
+	public String getPUSH() {
+		return (String) options.valueOf(OPTION_PUSH);
+	}
+	public String getSERVER() {
+		return (String) options.valueOf(OPTION_SERVER);
+	}
+	public  String getPORT() {
+		return (String) options.valueOf(OPTION_PORT);
+	}
+	public  String getCLIENTID() {
+		return (String) options.valueOf(OPTION_CLIENTID);
+	}
+}
--- pietrackr/src/main/java/se/murf/pietrackr/PieTrackr.java
+++ pietrackr/src/main/java/se/murf/pietrackr/PieTrackr.java
@@ -3,31 +3,28 @@ package se.murf.pietrackr;
 import java.io.IOException;
 import java.util.Date;
 
-import joptsimple.OptionException;
-import joptsimple.OptionParser;
-
-import joptsimple.OptionSet;
-
 /**
  * @author Murf Mellgren 20140414
  * daemon running on Raspberry pie for tracking GPS positions
  * https://trackrd.murf.se
  */
 public class PieTrackr  {
-	private static final String HELP_OPTION = "help";
-	private static final String OPTION_PUSH = "pushtopic";
-	private static final String OPTION_SERVER = "pushtopic";
-	private static final String OPTION_PORT = "pushtopic";
+	private static Configuration config;
+	private static Connect connection;
 	
-	public static void main( String[] args ) {
+	public static void main( String[] args ) throws Exception {
+		/**
+		 * Parse Configuration
+		 */
+		config = new Configuration(args);
+		/**
+		 * Connect to MQTT
+		 */
+		connection = new Connect(config);
 		
-		OptionParser parser = new OptionParser() {
-			{
-				accepts(HELP_OPTION, "Shows this help message.");
-				accepts(OPTION_PUSH, "Topic to push to.").withRequiredArg().describedAs("pushtopic").ofType(String.class);
-
-			}
-		};
+		/**
+		 * Send Message to MQTT
+		 */
 		
         System.out.println( "Hello World!" );
     }
