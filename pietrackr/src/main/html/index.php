7f3725783c9a346bc7b2ef6885cb1e265939adbf
Mikael Mellgren <mikael@murf.se>
2015-01-11 00:34:51 +0100

    Revamped the google maps JS. Shamelessly used: https://github.com/owntracks/contrib/tree/master/gmaps as a basis. Modified to work with my setup

--- pietrackr/src/main/html/index.php
+++ pietrackr/src/main/html/index.php
@@ -10,7 +10,7 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$query="SELECT * FROM tracking.raw ORDER BY id DESC LIMIT 1";
+$query="SELECT * FROM tracking.raw WHERE user='".$_GET["username"]."' ORDER BY id DESC LIMIT 1;";
 if(!$result = $conn->query($query)){
     die('There was an error running the query [' . $db->error . ']');
 }
@@ -19,14 +19,14 @@ while($row = $result->fetch_assoc()){
         $latitude=$row['latitude'];
         $longitude=$row['longitude'];
         $altitude=$row['altitude'];
-	$time_fmt="Time: ".gmdate("Y-m-d\T H:i:s\Z", $timestamp);
-        #echo "Time: " . gmdate("Y-m-d\TH:i:s\Z", $timestamp) . "Lat: " . $latitude . "Lon: " . longitude . "Alt: " . $altitude . "</ br>";
-    /**
-    device
-    user
-    topic
-    speed
-    **/
+        $user=$row['user'];
+        $topic="display/".$user."/web";
+		$markerinfo=gmdate("Y-m-d\T H:i:s", $timestamp)." (CET)<br /><b>".$user."</b><br />".$topic;
+  		 /**
+		device
+		user
+		speed
+		**/
 }
 ?>
 <!DOCTYPE html>
@@ -34,43 +34,30 @@ while($row = $result->fetch_assoc()){
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <style type="text/css">
-      html { height: 100% }
-      body { height: 100%; margin: 0; padding: 0 }
-      #map-canvas { height: 100% }
-    </style>
-    <script type="text/javascript"
-      src="lib/mqttws31.js">
-    </script>
-    <script type="text/javascript"
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC93yNqEP89_kQfdoRT8OIZUOAuOYGSDDs&sensor=true">
-    </script>
-    <script src="config.js" type="text/javascript"></script>
+		html, body, #map {
+			margin: 0;
+			padding: 0;
+			height: 100%;
+		}
+		#legend {
+			font-family: Arial, sans-serif;
+			background: #fff;
+			padding: 5px;
+			margin: 5px;
+			border: 1px solid #000;
+		}
+		#legend h3 {
+			margin-top: 0;
+		}
+		#legend img {
+			vertical-align: middle;
+		}
+	</style>
+    <script type="text/javascript" src="lib/mqttws31.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC93yNqEP89_kQfdoRT8OIZUOAuOYGSDDs&sensor=false"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="config.js" type="text/javascript"></script>
     <script type="text/javascript">
-		function initialize() {
-			map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
-			Location = {
-				lat:<?php echo $latitude;?>,
-				lon:<?php echo $longitude;?>,
-				title:'<?php echo $time_fmt;?>',
-				descr:'<?php echo $time_fmt;?>'           
-			};
-			marker = new google.maps.Marker({
-				map: map,
-				title: Location.title,
-				position: new google.maps.LatLng(Location.lat, Location.lon),           
-				icon: img
-			});
-			bindInfoWindow(marker, map, infowindow, "<p>" + Location.descr + "</p>",Location);  
-		}
-		function bindInfoWindow(marker, map, infowindow, html) { 
-			google.maps.event.addListener(marker, 'mouseover', function() {
-				infowindow.setContent(html); 
-				infowindow.open(map, marker); 
-			});
-			google.maps.event.addListener(marker, 'mouseout', function() {
-				infowindow.close();
-			}); 
-		}
 		
 		//MQTT Stuff 
 		function MQTTConnect() {
@@ -92,24 +79,27 @@ while($row = $result->fetch_assoc()){
 		}
 		function onMessageArrived(message) {
 			console.log("onMessageArrived:"+message.payloadString);
-			//59.187668333,17.618505,0.026,48.3,1398277504
-			//59.187676667,17.618515,1.09,48.4,1398277505
-			var data = message.payloadString;
-			var data2 = JSON.parse(data);
-			// Add Fuzzy later to update. aka minidiff 
-			console.log("Location.lat: " + Location.lat + " Location.lon: " + Location.lon);
-			console.log("Location.lat: " + data2.lat + " Location.lon: " + data2.lon);
-			console.log((Location.lat - data2.lat) * -1);
-			console.log((Location.lon - data2.lon) * -1);
-			if((Location.lat - data2.lat) * -1 > 0.00001 || (Location.lon - data2.lon) * -1 > 0.00001) {
-				console.log("Updating position");
-				Location.lat = data2.lat;
-				Location.lon = data2.lon;
-				Location.descr = "Speed: " + data2.speed + " Alt: " + data2.alt;
-				infowindow.setContent(Location.descr);
-				marker.setPosition( new google.maps.LatLng( Location.lat, Location.lon ) );
-		    		map.panTo( new google.maps.LatLng( Location.lat, Location.lon  ) );
-	    		}
+			//check to see if payload is JSON
+			try {
+				var msgjson = jQuery.parseJSON(message.payloadString); //parse payload
+			} catch (e) {
+				console.log(e);
+				return false;
+			};
+			console.log("Location.lat: " + msgjson.lat + " Location.lon: " + msgjson.lon);
+			var myDate = new Date(msgjson.tst *1000);
+			var MONTH=parseInt(myDate.getUTCMonth()) + 1;
+			if(MONTH<10) {
+				var parsed="0" + MONTH.toString();
+			} else {
+				var parsed=MONTH.toString();
+			}
+			var time=myDate.getUTCFullYear()+"-"+parsed+"-"+myDate.getUTCDate()+" "+myDate.getUTCHours()+":"+myDate.getUTCMinutes()+":"+myDate.getUTCSeconds()+" (CET)";
+			Location.descr = "Speed: " + msgjson.speed + " Alt: " + msgjson.alt;
+			var markerinfo = time+'<br /><b>'+msgjson.user+'</b><br />'+message.destinationName;
+			addMarker(msgjson.lat,msgjson.lon,markerinfo, msgjson.user); //add marker based on lattitude and longittude, using timestamp for description for now
+			center = bounds.getCenter(); //center on marker, zooms in to far atm, needs to be fixed!
+			map.fitBounds(bounds);
 		}
 		function onConnect() {
 			// Once a connection has been made, make a subscription and send a message.
@@ -161,37 +151,147 @@ while($row = $result->fetch_assoc()){
 		    }
 		
 		}
-		    	
+		
+		//Google Maps Stuff
+		function addMarker(lat, lng, info, person) {
+			//console.log('addmaker -- ' + lat + ' ' + lng + ' ' + info + ' ' + person);
+			
+			var pinColour = 'blue' //default colour of pin
+				
+			//check the array if the person is already added to the array, if not add it.
+			if (personArray.indexOf(person) < 0){
+				personArray.push(person); //add the new person to the array
+				pinColour = pinColourArray[personArray.indexOf(person)]; 				// add the new person to the legend on the map
+				var div = document.createElement('div');
+				div.innerHTML = '<img onclick="selectmarker('+ personArray.indexOf(person) +')" src="https://maps.google.com/mapfiles/ms/micons/' + pinColour + '.png"> ' + person;
+				legend.appendChild(div);
+				//create a new item for person
+				latestlocations.push({
+					key: 	personArray.indexOf(person),
+					value: 	0, 
+				});
+
+			}
+
+			//check the pincolour for the person
+			pinColour = pinColourArray[personArray.indexOf(person)];
+
+			//assign the marker colour
+			var icon = new google.maps.MarkerImage("https://maps.google.com/mapfiles/ms/micons/" + pinColour +".png",
+				   new google.maps.Size(32, 32), new google.maps.Point(0, 0),
+				   new google.maps.Point(16, 32));
+			var pt = new google.maps.LatLng(lat, lng);
+			bounds.extend(pt);
+			x = x + 1;
+			var marker = new google.maps.Marker({
+				position: pt,
+				icon: icon,
+				map: map,
+				id: x,
+		        animation: google.maps.Animation.DROP
+
+			});
+			
+			markers[x] = marker; //add to the markers array so we can call them later
+			latestlocations[personArray.indexOf(person)] =	markers[x]; //update array with their latest location
+
+			//create the popup for marker
+			var popup = new google.maps.InfoWindow({
+				content: info,
+				maxWidth: 400
+			});
+
+			//what happens when you click on a marker
+			google.maps.event.addListener(marker, "click", function() {
+				if (currentPopup != null) {
+					currentPopup.close();
+					currentPopup = null;
+				}
+				
+				//zoom in on the marker
+				
+    			map.setCenter(marker.getPosition());
+    			//open the info box popup
+				popup.open(map, marker);
+				var listener = google.maps.event.addListener(map, "idle", function() { 
+					if (map.getZoom() > 15) map.setZoom(15); 
+					google.maps.event.removeListener(listener); 
+				});
+				currentPopup = popup;
+			});
+
+			//what happens when you close a marker
+			google.maps.event.addListener(popup, "closeclick", function() {
+				//zoom back out to see all markers
+				center = bounds.getCenter();
+		    	map.fitBounds(bounds);
+				//map.panTo(center);
+				currentPopup = null;
+			});
+
+			//zoom in on this latest marker
+			selectmarker(personArray.indexOf(person));
+		};
+		//function for selecting the latest marker for a persion
+		function selectmarker(person) {
+    		google.maps.event.trigger(latestlocations[person], 'click');
+		};
+		function initMap() {
+			map = new google.maps.Map(document.getElementById("map"), {
+				center: { lat: <?php echo $latitude;?>, lng: <?php echo $longitude;?>},
+				zoom: 15,
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				mapTypeControl: true,
+				mapTypeControlOptions: {
+					style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
+				},
+				navigationControl: true,
+				navigationControlOptions: {
+					style: google.maps.NavigationControlStyle.ZOOM_PAN
+				}
+			});
+			
+			var legend = document.getElementById('legend');
+
+		    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
+
+			//center = bounds.getCenter();
+		    //map.fitBounds(bounds);
+		    <?php
+		    /**
+		    $timestamp
+			$latitude
+			$longitude
+			$altitude
+			$time_fmt
+			$user
+			device
+			topic
+			speed
+			**/
+			echo "addMarker(".$latitude.",".$longitude.",'".$markerinfo."','".$user."');";
+		    ?>
+		    //addMarker(msgjson.lat,msgjson.lon,markerinfo, message.destinationName);
+		    MQTTConnect();
+		};
 		
 		//Start The Goddamn Program
 		var username = getURLParameters("username");
 		var password = getURLParameters("password");
-		MQTTConnect();
 		
-		var size=19;        
-		var img=new google.maps.MarkerImage('marker.png',           
-			new google.maps.Size(size, size),
-			new google.maps.Point(0,0),
-			new google.maps.Point(size/2, size/2)
-		);
-		var infowindow =  new google.maps.InfoWindow({
-			content: ''
-		});
-		var mapOptions = {
-			center: new google.maps.LatLng(<?php echo $latitude.",".$longitude;?>),
-			zoom: 17,
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-			
-	    };
-	    var map;
-	    var Location;
-	    var marker;
-	    var infowindow;
-		google.maps.event.addDomListener(window, 'load', initialize);
-		
+		var personArray = new Array();
+		var pinColourArray = new Array('red','blue','yellow','pink','green','lightblue','orange','purple','red-dot','blue-dot','yellow-dot','pink-dot','green-dot','lightblue-dot','orange-dot','purple-dot');
+		var center = null;
+		var map = null;
+		var currentPopup;
+		var bounds = new google.maps.LatLngBounds();
+		var x = 0;
+		var markers = {};
+		var latestlocations = []; // creating dictionary of latest locations.
     </script>
   </head>
-  <body>
-    <div id="map-canvas"/>
+ <body onload="initMap()" >
+		<div id="map"></div>
+		<div id="legend"><h3>Device/User</H3></div>
   </body>
 </html>
